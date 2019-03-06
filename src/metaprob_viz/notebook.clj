c78c7711b20f487d82fea3b15142a3f421281e1f
Joshua Thayer <jthayer@mit.edu>
2019-03-06 15:23:07 -0800

    workaround static html issue

--- src/metaprob_viz/notebook.clj
+++ src/metaprob_viz/notebook.clj
@@ -1,5 +1,6 @@
 (ns metaprob-viz.notebook
   (:require [metaprob-viz.viz :as viz]
+            [clojure.java.browse :as browse]
             [clojupyter.misc.display :as display]))
 
 (defn open-in-notebook [v-id & [h]]
@@ -10,6 +11,14 @@
                :width "100%"
                :height height}])))
 
+(defn open-in-tab [v-id]
+  (display/hiccup-html
+   [:a {:href (viz/viz-url v-id)
+        :target "_blank"}
+    "Click to open visualization window"]))
+
 (defn display-in-notebook [f v-id & [h]]
-  (open-in-notebook v-id h)
+  ;; (browse/browse-url (viz/viz-url v-id))
+  ;; (open-in-notebook v-id h)
+  (f)
   (display/html (viz/get-html v-id)))
